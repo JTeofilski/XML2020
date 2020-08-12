b3ec2a646c4be1340438d9f959ca9cab7fa6c399
VioletaMarceta <violetamarceta1995@gmail.com>
2020-08-12 14:22:36 +0200

    rad na dodavanju zahteva

--- ZahtevZaIznajmljivanje/src/main/java/com/zahtevazaiznajmljivanje/ZahtevZaIznajmljivanje/controller/ZahtevZaIznajmljivanjeController.java
+++ ZahtevZaIznajmljivanje/src/main/java/com/zahtevazaiznajmljivanje/ZahtevZaIznajmljivanje/controller/ZahtevZaIznajmljivanjeController.java
@@ -1,5 +1,6 @@
 package com.zahtevazaiznajmljivanje.ZahtevZaIznajmljivanje.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zahtevazaiznajmljivanje.ZahtevZaIznajmljivanje.model.ZahtevZaIznajmljivanje;
+import com.zahtevazaiznajmljivanje.ZahtevZaIznajmljivanje.model.Agent;
 import com.zahtevazaiznajmljivanje.ZahtevZaIznajmljivanje.model.Oglas;
 import com.zahtevazaiznajmljivanje.ZahtevZaIznajmljivanje.service.ZahtevZaIznajmljivanjeService;
 
@@ -33,32 +35,73 @@ public class ZahtevZaIznajmljivanjeController {
 	
 	@RequestMapping(method=RequestMethod.POST, value = "kreiranje/{bundle}", consumes="application/json")
 	public ResponseEntity <ZahtevZaIznajmljivanje> kreiranjeZahteva(@PathVariable("bundle") boolean bundle, @RequestBody List<Oglas> oglasi){
-		
+		ArrayList<Agent> agenti = new ArrayList<>();
 		ZahtevZaIznajmljivanje z = null;
+		ArrayList<Agent> agentiNovo = new ArrayList<>();
 		if(bundle==true) {
+			for (int i=0;i<oglasi.size();i++) {
+				  agenti.add(oglasi.get(i).getAgent()); //stavlja sve agente u posebnu listu
+			}
+			
+			for(int j=0; j<agenti.size(); j++) {
+				
+				
+				if(agentiNovo.size()==0){
+					agentiNovo.add(agenti.get(j));
+					
+				}
+				
+			
+				    if(!agentiNovo.contains(agenti.get(j))) {
+						//algoritam za trazenje duplikata...mozda moze ovako
+						agentiNovo.add(agenti.get(j));
+					
+				
+					
+				}
+			}
+			for(int i=0; i<agentiNovo.size(); i++) {
+			System.out.println("agent novo id " + agentiNovo.get(i).getIdentifikacioniBroj());
+			}
+			List<List<Oglas>> setovi = new ArrayList<List<Oglas>>();
+			List<ZahtevZaIznajmljivanje> zahtevi = new ArrayList<ZahtevZaIznajmljivanje>();
+			for(int i=0; i<agentiNovo.size(); i++) {
+				System.out.println("ima li necega "  + agentiNovo.get(i).getIme());
+				List<Oglas> list = new ArrayList<Oglas>(); 
+			    //ova lista posle mora da se konvertuje u set
+				setovi.add(list);
+				ZahtevZaIznajmljivanje zahtev = new ZahtevZaIznajmljivanje();
+				
+			    zahtevi.add(zahtev);
+			} //za svakog agenta pravi njegove posebne zahteve i liste u koje ce posle staviti oglase
+			//posle se pristupa zahtev.get(k), gde je k broj agenta u listi 
 			
 			for (int i=0;i<oglasi.size();i++) {
-				z= new ZahtevZaIznajmljivanje();
-			
-				for (int j=1;j<oglasi.size();j++) {
-					if(oglasi.get(i).getAgent().getIme().equals(oglasi.get(j).getAgent().getIme())) {
-						if(z.getOglasi()== null) {
-							Set<Oglas> set = new HashSet<>();
-							set.add(oglasi.get(j));
-							z.setOglasi(set);	
+				//z= new ZahtevZaIznajmljivanje();
+				for(int k=0; k<agentiNovo.size(); k++) {
+				//for (int j=1;j<oglasi.size();j++) {
+					if(oglasi.get(i).getAgent().equals(agentiNovo.get(k))) {
+						if(zahtevi.get(k).getOglasi()== null) {
+							//Set<Oglas> set = new HashSet<>();
+							setovi.get(k).add(oglasi.get(i));
+							Set<Oglas> targetSet = new HashSet<>(setovi.get(k));//konvertujem listu u set
+							zahtevi.get(k).setOglasi(targetSet); //dodaju se bas u zahtev agenta k	
 						}
 						else {
 							System.out.println("isti agenti");
-							z.getOglasi().add(oglasi.get(j));			
-							oglasi.remove(j);
+							zahtevi.get(k).getOglasi().add(oglasi.get(i));			
+							oglasi.remove(i);
 						
 						}
 					}
+				//}
 				}
-				z.getOglasi().add(oglasi.get(i));
-				z.setAgentFirmaID(oglasi.get(i).getAgent().getIdentifikacioniBroj());
-				z.setStatusIznajmljivanja("PENDING");
-				zahtevService.save(z);
+				//z.getOglasi().add(oglasi.get(i));
+				//z.setAgentFirmaID(oglasi.get(i).getAgent().getIdentifikacioniBroj());
+				//z.setStatusIznajmljivanja("PENDING");
+				for(int m=0; m<zahtevi.size(); m++) {
+				zahtevService.save(zahtevi.get(m));
+				}
 			}
 		}
 		else {
